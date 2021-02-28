a507e0072436cb04dc3a08f02bdd4771a6d7c018
Vuong Nguyen <nguyenvuong1122000@gmail.com>
2021-02-28 21:28:09 +0700

    init

--- predict.py
+++ predict.py
@@ -2,11 +2,11 @@ from vietocr.tool.config import Cfg
 from vietocr.model.trainer import Trainer
 from vietocr.tool.predictor import Predictor
 from PIL import Image
-
+import numpy as np
 config = Cfg.load_config_from_name('vgg_transformer')
 
-config['weights'] = '/model/weight.pth'
-config['cnn']['pretrained']=False
+config['weights'] = '/home/vuong/PycharmProjects/hand-predict/model/weight.pth'
+config['cnn']['pretrained'] = True
 config['device'] = 'cpu'
 config['predictor']['beamsearch']=False
 
